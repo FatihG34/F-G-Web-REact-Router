c982bd65e5fd6e557501f0f4f7510122d9547998
FatihG34 <fthgnydn34@gmail.com>
2022-08-19 17:19:05 +0300

    navs modified

--- src/components/navbar/NavSubscribe.jsx
+++ src/components/navbar/NavSubscribe.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const NavSubscribe = () => {
     return (
-        <div>NavSubscribe</div>
+        <nav className="navbar" style={{ backgroundColor: "#35414A" }}>
+            <div className="container-fluid justify-content-around">
+                <h3 className="navbar-brand text-white">Subscribe To Our Newsletter</h3>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="email" placeholder="Enter email..." aria-label="email" />
+                    <button className="btn btn-outline-success bg-warning text-white" type="submit">Subscribe</button>
+                </form>
+            </div>
+        </nav>
     )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,29 +3,23 @@ import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark text-white">
+        <nav className="navbar navbar-expand-lg text-white" style={{ backgroundColor: "#35414A", borderBottom: '2px solid green' }}>
             <div className="container-fluid">
-                <Link className="navbar-brand text-white" to={{ pathname: 'https://clarusway.com/' }} target="_blank">Clarusway</Link>
+                <Link className="navbar-brand text-success" to={{ pathname: 'https://clarusway.com/' }} target="_blank"><h2>Clarusway <span className='text-white'>Web Design</span></h2></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active text-white" aria-current="page" to="/" style={isActive => ({
-                                bgColor: isActive ? "green" : "blue"
-                            })}>Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/about" style={isActive => ({
-                                bgColor: isActive ? "green" : "blue"
-                            })}>About</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/services" style={isActive => ({
-                                bgColor: isActive ? "green" : "blue"
-                            })}>Services</NavLink>
-                        </li>
+                    <ul className="navbar-nav mx-5">
+                        <NavLink className="nav-link active" aria-current="page" to="/" style={({ isActive }) => ({
+                            color: isActive ? "yellow" : "white"
+                        })}>Home</NavLink>
+                        <NavLink className="nav-link mx-5" to="/about" style={({ isActive }) => ({
+                            color: isActive ? "yellow" : "white"
+                        })}>About</NavLink>
+                        <NavLink className="nav-link" to="/services" style={({ isActive }) => ({
+                            color: isActive ? "yellow" : "white"
+                        })}>Services</NavLink>
                     </ul>
                 </div>
             </div>
